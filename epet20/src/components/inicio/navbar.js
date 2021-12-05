6243d58a738fcf8922c526899fa05ff9105230e3
Ezequiel López <contacto.ezequiel.lopez@gmail.com>
2021-12-04 21:38:36 -0300

    añadido de efecto "bold" a navbar

--- epet20/src/components/inicio/navbar.js
+++ epet20/src/components/inicio/navbar.js
@@ -21,13 +21,13 @@ function Navbar() {
                     <div><button className="btn main-color button-menu shadow-md p-2 m-2">|||</button></div>
                     <Link to="/inicio" className="m-2 ">  <img src={Icon} className="shadow-xl rounded-xl hidden-title" alt="Logo" /> </Link>
                     <div className="m-3">
-                        <Link to="/inicio" class="nav-color menu-item  p-2 mb-4">E.P.E.T N° 20</Link>
-                        <Link to="/novedades" class="nav-color menu-item  p-2 mb-4">Novedades</Link>
-                        <Link to="/estudiantes" class="nav-color menu-item  p-2 mb-4">Estudiantes</Link>
-                        <Link to="/secretaria" class="nav-color menu-item  p-2  mb-4">Secretaria</Link>
-                        <Link to="/contacto" class="nav-color menu-item  p-2 mb-4">Contacto</Link>
-                        <Link to="/plan-de-estudios" class="nav-color menu-item p-2 mb-4">Plan de estudios</Link>
-                        <a href="https://regular.neuquen.gob.ar/Inscripciones2021/servlet/com.certiregu.verificatramite" class="nav-color menu-item p-2 mb-4">¿Estoy inscripto?</a>
+                        <Link to="/inicio" class="nav-color font-bold menu-item  p-2 mb-4">E.P.E.T N° 20</Link>
+                        <Link to="/novedades" class="nav-color font-bold menu-item  p-2 mb-4">Novedades</Link>
+                        <Link to="/estudiantes" class="nav-color font-bold menu-item  p-2 mb-4">Estudiantes</Link>
+                        <Link to="/secretaria" class="nav-color font-bold menu-item  p-2  mb-4">Secretaria</Link>
+                        <Link to="/contacto" class="nav-color font-bold menu-item  p-2 mb-4">Contacto</Link>
+                        <Link to="/plan-de-estudios" class="nav-color font-bold menu-item p-2 mb-4">Plan de estudios</Link>
+                        <a href="https://regular.neuquen.gob.ar/Inscripciones2021/servlet/com.certiregu.verificatramite" class="nav-color font-bold menu-item p-2 mb-4">¿Estoy inscripto?</a>
                     </div>
                 
 
