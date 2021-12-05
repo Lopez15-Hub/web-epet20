e5c8003a97ca10ded5c0ba65557b6c31c1360ecc
Ezequiel López <contacto.ezequiel.lopez@gmail.com>
2021-12-04 21:54:14 -0300

    Añadido de lineas de colectivo

--- epet20/src/views/contacto.js
+++ epet20/src/views/contacto.js
@@ -63,18 +63,18 @@ export const Contacto = () => {
                         <div className="d-flex ">
                             <div className="col-8">
                                 <div className="col-xs-12 col-sm-12 col-md-12 col-xl-12 "><Subtitle text="Dirección del lugar" /></div>
-                                <a href="https://www.google.com.ar/maps/place/Escuela+Provincial+de+Educaci%C3%B3n+T%C3%A9cnica+N%C2%B0+20/@-38.9648652,-68.0899264,17z/data=!3m1!4b1!4m5!3m4!1s0x960a33b795c819a7:0x65c00b69eb2c3f1d!8m2!3d-38.9647969!4d-68.0877371" className="col-xs-12 col-sm-12  col-md-4 col-xl-4  ">Lanin 2020</a>
+                                <a href="https://www.google.com.ar/maps/place/Escuela+Provincial+de+Educaci%C3%B3n+T%C3%A9cnica+N%C2%B0+20/@-38.9648652,-68.0899264,17z/data=!3m1!4b1!4m5!3m4!1s0x960a33b795c819a7:0x65c00b69eb2c3f1d!8m2!3d-38.9647969!4d-68.0877371" className="col-xs-12 col-sm-12  col-md-4 col-xl-4  ">Neuquén Capital (8300) - Lanin 2020</a>
                             </div>
                             <div className="col-6">
                                 <div className="col-xs-12 col-sm-12 col-md-12 col-xl-12 "><Subtitle text="Lineas de colectivo" /></div>
-                                <p>Linea 4</p> <p>Linea 5B</p> <p>Linea 5A</p>
+                                <p>Linea 4</p> <p>Linea 5B</p> <p>Linea 5A</p><p>Linea 50A</p><p>Linea 50B</p>
 
                             </div>
                         </div>
                     </div>
-                   <div className="mt-2 mb-2">
-                   <Title text="Contacto programador" />
-                   </div>
+                    <div className="mt-2 mb-2">
+                        <Title text="Contacto programador" />
+                    </div>
 
                     <div>
                         <div className="d-flex ">
