92092502773ce4c40ee3eda5d7985b0da9c01e9d
Ezequiel López <contacto.ezequiel.lopez@gmail.com>
2021-12-06 04:38:37 -0300

    bordes redondeados a componente "card"

--- epet20/src/components/inicio/card.js
+++ epet20/src/components/inicio/card.js
@@ -1,6 +1,6 @@
 import React from "react";
 const MyCard = ({imageUrl, description,alt})=> {
-    return <div className="card w-18 shadow-md " >
+    return <div className="card w-18 rounded-xl shadow-md " >
          <img src={imageUrl} className="img" alt={alt}/>
         <div className ="card-body">
         <p className ="card-text text-center">{description}</p>
