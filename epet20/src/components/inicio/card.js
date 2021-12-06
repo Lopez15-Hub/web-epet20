d0b70f00800017030b3b79f26c96dca41be38b8d
Ezequiel López <contacto.ezequiel.lopez@gmail.com>
2021-12-06 05:07:33 -0300

    Añadido de animaciones a las cards de signatures. Mejora en el aspecto visual del inicio

--- epet20/src/components/inicio/card.js
+++ epet20/src/components/inicio/card.js
@@ -1,6 +1,6 @@
 import React from "react";
 const MyCard = ({imageUrl, description,alt})=> {
-    return <div className="card w-18 rounded-xl shadow-md " >
+    return <div className="card w-18 rounded-b-xl shadow-md " >
          <img src={imageUrl} className="img" alt={alt}/>
         <div className ="card-body">
         <p className ="card-text text-center">{description}</p>
