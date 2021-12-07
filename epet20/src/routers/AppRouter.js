fa855ed1ff82f0c6f897bede85eef1a0af2729fc
Ezequiel López <contacto.ezequiel.lopez@gmail.com>
2021-12-06 22:15:32 -0300

    Configuración de rutas en sección secretaria

--- epet20/src/routers/AppRouter.js
+++ epet20/src/routers/AppRouter.js
@@ -4,7 +4,7 @@ import { Contacto } from '../views/contacto';
 import { Estudiantes } from '../views/estudiantes';
 import { Inicio } from '../views/inicio';
 import { Novedades } from '../views/novedades';
-import { AnimatePresence} from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import { Login } from '../views/login';
 import { Registro } from '../views/registro';
 import { Secretaria } from '../views/secretaria';
@@ -15,11 +15,15 @@ export const AppRouter = () => {
         <BrowserRouter>
             <AnimatePresence>
                 <Routes>
+                    <Route path="/error" element={<Inicio />} />
                     <Route path="/" element={<Inicio />} />
                     <Route path="/inicio" element={<Inicio />} />
                     <Route path="/novedades" element={<Novedades />} />
                     <Route path="/estudiantes" element={<Estudiantes />} />
                     <Route path="/secretaria" element={<Secretaria />} />
+                    <Route path="/secretaria/estudiantes" element={<Secretaria />} />
+                    <Route path="/secretaria/teoria" element={<Secretaria />} />
+                    <Route path="/secretaria/docentes" element={<Secretaria />} />
                     <Route path="/contacto" element={<Contacto />} />
                     <Route path="/plan-de-estudios" element={<PlanDeEstudios />} />
                     <Route path="/login" element={<Login />} />
