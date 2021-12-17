3dc911e047c02427a942f855570d9c0289868a28
Tomás <ttomycastro@gmail.com>
2021-12-17 14:37:36 -0300

    Sección novedades v1
    Temporalmente finalizada.

--- epet20/src/components/novedades/Advertisements.js
+++ epet20/src/components/novedades/Advertisements.js
@@ -8,6 +8,7 @@ export default function Advertisements () {
         <>
                 <Row  className="justify-content-md-center">
                     <Card style={{ width: '50rem' }}>
+                    <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/5/59/%C3%8Dcono_Computadora_-_Internet.JPG" />
                             <Card.Body>
                             <Card.Title className="text-center">Anuncios</Card.Title>
                             <Card.Text className="text-center">
