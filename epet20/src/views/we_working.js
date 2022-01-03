72b5bc7d83c0ebb5b171a4630c17bfb22fc0719a
Ezequiel López <contacto.ezequiel.lopez@gmail.com>
2022-01-03 19:00:01 -0300

    Spinner en weworking

--- epet20/src/views/we_working.js
+++ epet20/src/views/we_working.js
@@ -3,6 +3,7 @@ import { Subtitle } from '../components/text-styles/subtitle'
 import { Title } from '../components/text-styles/title'
 import Image from '../assets/weWorking.jpg';
 import { motion } from 'framer-motion';
+import { Loading } from '../components/general/loading';
 export const PaginaEnConstruccion = () => {
     return (
         <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
