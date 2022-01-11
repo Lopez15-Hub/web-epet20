f32b432fde5f3b964f234f2d0160173ce5624844
Ezequiel López <contacto.ezequiel.lopez@gmail.com>
2022-01-11 06:04:18 -0300

    Terminación de vista plan de estudios

--- epet20/src/views/plan_estudios.js
+++ epet20/src/views/plan_estudios.js
@@ -1,56 +1,108 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { motion } from 'framer-motion'
 import { Title } from '../components/text-styles/title'
-import Footer from '../components/inicio/footer'
-import Section from '../components/inicio/section'
+
 import { Materias } from '../components/plan_estudios/table/materias'
 import { Subtitle } from '../components/text-styles/subtitle'
-import { FaDownload, FaPrint } from 'react-icons/fa'
+import { FaPrint } from 'react-icons/fa'
+import ReactToPrint from 'react-to-print';
+import { Container, Row } from 'reactstrap'
+import Footer from '../components/inicio/footer';
 export const PlanDeEstudios = () => {
+    const [visible, setVisible] = React.useState(true)
+    const ref = useRef(null)
+    const printDoc = () => {
+        window.print()
+
+
+
+    }
+
     return (
         <>
             {/*Barra de navegación*/}
+            <div fluid className='mt-2 mb-4'>
 
 
-            <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="container shadow-xl bg-white rounded-xl p-6 " >
+                <ReactToPrint
+                    trigger={() => <button onClick={() => printDoc()} className="btn btn-outline-warning ml-4 "><p p className='d-flex'> <FaPrint className='mr-2' />Imprimir plan de estudios</p> </button>}
+                    content={() => ref.current}
+                />
 
-                <div className="row">
 
-                    <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
-                        <div className="mb-4">
-                            <Title text="Plan de estudios" />
-                            <Subtitle text="Técnico en programación - Plan 682" />
+            </div>
+
+            <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} className=" mt-4  " >
+                <Container>
+                    <div className='row' ref={ref}>
+                        <div className='col-1'></div>
+                        <div className="row p-7 col-10" >
+                            <header className="bg-main-color shadow-md rounded-xl p-3">
+
+                                <p className="text-white text-center  uppercase font-bold mt-2">Escuela provincial de educación técnica número 20 </p>
+
+
+                            </header>
+                            <div className="col-xs-12 col-sm-12 col-md-8 col-lg-12 col-xl-12">
+                                <div className="mb-4">
+                                    <Title text="Plan de estudios" />
+                                    <Subtitle text="Título: Técnico en programación - Plan 682" />
+                                </div>
+                                <div>
+                                    <div className="font-bold">
+                                        <Subtitle text="Perfil del egresado" />
+                                        <p className='text-justify'>El Técnico en Programación participa en proyectos de desarrollo de software desempeñando roles que tienen por objeto analizar, diseñar, desarrollar e implementar sistemas informáticos.</p>
+                                    </div>
+                                    <div className="font-bold">
+                                        <Subtitle text="Alcance del título" />
+                                        <p className='text-justify'>La actividad de un técnico en programación se basa en el constante aprendizaje de las tecnologías implementadas, la compresión, el planteamiento y la resolución de un problema, la constante documentación del proyecto, aplicación y estructuración de metodologías ágiles que optimicen los tiempos de producción y aplicación de normativas que gestionen y aseguren la calidad del producto.</p>
+                                    </div>
+                                </div>
+                                {/*Stack de materias*/}
+                                <div >
+                                    <div className="mt-4">
+                                        <Title text="Listado de materias" />
+                                    </div>
+
+                                    <div >
+                                        <Subtitle text="Ciclo Básico" />
+                                        <Materias />
+                                        <Subtitle text="Ciclo Superior" />
+                                        <Materias />
+
+
+
+                                    </div>
+                                </div>
+
+
+
+
+
+                            </div>
+
+
+
+
+                            <footer className="bg-main-color shadow-md rounded-xl p-3">
+
+                                <p className="text-white text-center  uppercase font-bold mt-2">Consejo provincial de educación - Instituto nacional de educación tecnológica</p>
+                                <p className="text-white text-center ">Ciudad del Neuquén </p>
+                            </footer>
                         </div>
-
-                        {/*Stack de materias*/}
-                        <div className="mt-4">
-                            <Title text="Listado de materias" />
-                        </div>
-                        <div>
-                            <Subtitle text="Ciclo Básico" />
-                            <Materias />
-                            <Subtitle text="Ciclo Superior" />
-                            <Materias />
-
-
-
-                        </div>
-
-                        <button className="col-4 btn btn-primary  mr-4 "><p className='d-flex' ><FaDownload className='mr-2' />Descargar plan de estudios</p> </button>
-                        <button className="col-4 btn btn-outline-warning ml-4 "><p p className='d-flex'> <FaPrint className='mr-2' />Imprimir materias</p> </button>
+                        <div className='col-1'></div>
 
                     </div>
 
-                    <Section />
+                </Container>
 
 
-
-                </div>
+                {/*FOOOTER*/}
+                <Footer />
 
 
             </motion.div>
-            {/*FOOOTER*/}
-            <Footer />
+
         </>
     )
 }
