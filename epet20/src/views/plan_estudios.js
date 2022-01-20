ab615ec29c98cd9a7f1c90e9c9c47b70f97ac7ba
Ezequiel López <65269387+Lopez15-Hub@users.noreply.github.com>
2022-01-20 04:08:42 -0300

    Cambios finales

--- epet20/src/views/plan_estudios.js
+++ epet20/src/views/plan_estudios.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, {useRef } from 'react'
 import { motion } from 'framer-motion'
 import { Title } from '../components/text-styles/title'
 
