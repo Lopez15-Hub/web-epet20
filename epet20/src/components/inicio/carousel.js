5eb1dc6bb62629e5d5a524f5e821eab0e8b24830
Ezequiel López <65269387+Lopez15-Hub@users.noreply.github.com>
2022-01-21 05:07:09 -0300

    Carousel dinámico terminado

--- epet20/src/components/inicio/carousel.js
+++ epet20/src/components/inicio/carousel.js
@@ -32,36 +32,16 @@ export default function MyCarousel() {
   }, [setLoading]);
   return <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
     {imagesFiles.length !== 0 ? <>
-      <div className="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-
-      </div>
       <div className="carousel-inner rounded-lg shadow-md">
+        {
+          imagesFiles.map(image => <div className={imagesFiles[0].id === image.id ? "carousel-item active" : 'carousel-item'} key={image.id} >
+            <img src={image.url} className="img-carousel d-block w-100 " alt="frente de la epet20" />
+            <div className="carousel-caption font-bold  rounded-xl shadow-xl text-center nav-background d-none d-md-block">
+              <h5>{image.description}</h5>
+            </div>
+          </div>)
+        }
 
-
-        <div className="carousel-item active">
-          <img src={imagesFiles[0].url} className="img-carousel d-block w-100 " alt="frente de la epet20" />
-          <div className="carousel-caption font-bold  rounded-xl shadow-xl text-center nav-background d-none d-md-block">
-            <h5>{imagesFiles[0].description}</h5>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img src={imagesFiles[1].url} className=" img-carousel d-block w-100 " alt="inagurando la escuela" />
-          <div className="carousel-caption font-bold nav-background rounded-xl shadow-xl text-center  d-none d-md-block">
-            <h5>{imagesFiles[1].description}</h5>
-
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img src={imagesFiles[2].url} className="img-carousel d-block  w-100" alt="inagurando la escuela" />
-          <div className="carousel-caption font-bold nav-background rounded-xl shadow-xl text-center d-none d-md-block">
-            <h5>{imagesFiles[2].description}</h5>
-
-          </div>
-
-        </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
