d52eb031ae5f959b3119a7f25c070310caa169a7
Ezequiel López <65269387+Lopez15-Hub@users.noreply.github.com>
2022-01-26 16:16:25 -0300

    Implementación de firebase admin

--- epet20/src/firebase/admin.js
+++ epet20/src/firebase/admin.js
@@ -2,7 +2,14 @@
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./firebase-keys.json");
+try {
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+
+    });
+} catch (e) {
+    console.log(e);
+}
+export const firebaseAuthAdmin = admin.auth();
+export const firestoreAdmin = admin.firestore();
