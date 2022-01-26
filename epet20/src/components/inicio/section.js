e97f5a11f90770b12177b9e66e23bdaad42c8b50
Ezequiel López <65269387+Lopez15-Hub@users.noreply.github.com>
2022-01-26 02:51:32 -0300

    Eliminación de archivos innecesarios -Corrección de errores y mejoras

--- epet20/src/components/inicio/section.js
+++ epet20/src/components/inicio/section.js
@@ -33,21 +33,20 @@ export default function Section() {
     return <section className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 main-color text-bold text-justify">
         <Title text="Un poco de nuestra historia" />
         <img className="img-fluid text-center mx-auto w-50 mt-4 mb-5" src={Image} alt="Logo EPET N°20" />
-        <p>
-            {data.presentacion ? data.presentacion : <LoadingSpinner />}
 
-        </p>
+        {data.presentacion ? <p> {data.presentacion}  </p> : <LoadingSpinner />}
+
+
         <div className="mt-4">
             <Title text="Perfil del egresado" />
         </div>
         <h5 className="font-bold mb-2 mt-2">¿Qué implica ser técnico en programación? </h5>
-        <p className="text-justify">
-            {data.perfilTecnico ? ' ' + data.perfilTecnico : <LoadingSpinner />}
 
-        </p>
+        {data.perfilTecnico ? <p className="text-justify"> {' ' + data.perfilTecnico}      </p> : <LoadingSpinner />}
+
+
         <h5 className="font-bold mb-2 mt-4">¿Qué voy a poder realizar? </h5>
-        <p className="text-justify">
-            {data.alcances ? data.alcances : <LoadingSpinner />}        </p>
+        <p className="text-justify">{data.alcances ? data.alcances : <LoadingSpinner />} </p>
         <Link to="/plan-de-estudios" className=" text-button text-center font-bold btn btn-sm btn-block text-large mt-4 mb-4">Ver más</Link>
     </section>
 
